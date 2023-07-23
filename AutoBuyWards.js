8350aa9a383fe743f230b1c2d5fc623b3e8b7da3
Frezidew <bertovroman2006@gmail.com>
2023-07-23 09:49:43 +0300

    Update AutoBuyWards.js

--- AutoBuyWards.js
+++ AutoBuyWards.js
@@ -1,24 +1,30 @@
-var AutoBuyWards;
-(function (AutoBuyWards) {
-    AutoBuyWards.exampleScript = {};
-    AutoBuyWards.exampleScript.OnScriptLoad = function () {
-        console.log('AutoBuyWards complite');
-    };
-    var IsAutoBuyActive = Menu.AddToggle(['Frezidew'], 'AutoBuyWards', true);
-    AutoBuyWards.exampleScript.OnUpdate = function () {
-        if (!GameRules.IsActiveGame) {
-            return;
-        }
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.exampleScript = void 0;
+var AutoBW = {};
+exports.exampleScript = {};
+exports.exampleScript.OnScriptLoad = function () {
+    console.log('AutoBuyWards complite');
+};
+var IsAutoBuyActive = Menu.AddToggle(['Frezidew'], 'AutoBuyWards', true);
+exports.exampleScript.OnUpdate = function () {
+    if (!GameRules.IsActiveGame) {
+        return;
+    }
+    AutoBW.OnUpdate = function () {
         var localHero = EntitySystem.GetLocalHero();
         var localPlayer = EntitySystem.GetLocalPlayer();
-        if (GameRules.CanPurchaseItem('item_ward_observer')) {
-            localPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_PURCHASE_ITEM, null, null, 42, Enum.PlayerOrderIssuer
-                .DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, localHero);
-        }
-        if (GameRules.CanPurchaseItem('item_ward_sentry')) {
-            localPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_PURCHASE_ITEM, null, null, 42, Enum.PlayerOrderIssuer
-                .DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, localHero);
+        var obsWard = GameRules.CanPurchaseItem('item_ward_observer');
+        if (Engine.OnceAt(1)) {
+            if (obsWard) {
+                localPlayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_PURCHASE_ITEM, null, null, 42, Enum.PlayerOrderIssuer
+                    .DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, localHero);
+                console.log("Ward buy");
+            }
+            else {
+                console.log("don't have obs ward");
+            }
         }
     };
-})(AutoBuyWards || (AutoBuyWards = {}));
-RegisterScript(AutoBuyWards.exampleScript);
+};
+RegisterScript(AutoBW, "AutoBW");
