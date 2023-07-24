9d37565ee8d73a7b33bd4042fe646bbd52f7ca2e
Frezidew <bertovroman2006@gmail.com>
2023-07-24 10:45:55 +0300

    Update AutoLotus.js

--- AutoLotus.js
+++ AutoLotus.js
@@ -4,24 +4,20 @@ var AutoLotus;
     AutoLotus.exampleScript.OnScriptLoad = function () {
         console.log('AutoLotosFrez complite');
     };
-    var truefalse;
     var AutoLotos = Menu.AddToggle(['Frezidevv'], 'AutoLotos', true);
-    AutoLotos.OnChange(function (state) {
-        truefalse = state.newValue;
-    });
+    var HealthLotus = Menu.AddSlider(['Frezidevv'], 'Хп для активации', 10, 1000, 100, 10);
     AutoLotus.exampleScript.OnUpdate = function () {
-        if (!truefalse) {
+        if (AutoLotos.GetValue() != true) {
             return;
         }
     };
-    var HealthLotus = Menu.AddSlider(['FrzScript'], 'Хп для активации', 10, 1000, 100, 10);
     AutoLotus.exampleScript.OnUpdate = function () {
         if (!GameRules.IsActiveGame) {
             return;
         }
         var localHero = EntitySystem.GetLocalHero();
         var localPlayer = EntitySystem.GetLocalPlayer();
-        var lotus = localHero.GetItem("item_famango", true) ? localHero.GetItem("item_famango", true) : localHero.GetItem("item_greater_famango", true) ? localHero.GetItem("item_great_famango", true) : localHero.GetItem("item_greater_famango", true);
+        var lotus = localHero.GetItem("item_famango", true) ? localHero.GetItem("item_famango", true) : localHero.GetItem("item_great_famango", true) ? localHero.GetItem("item_greater_famango", true) : localHero.GetItem("item_famango", true);
         if (!localPlayer || !localHero) {
             return;
         }
