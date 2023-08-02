71ee1adf11cd19c891e3a6447898c45fd9971928
Frezidew <bertovroman2006@gmail.com>
2023-08-02 09:28:26 +0300

    Update Predict.js

--- Predict.js
+++ Predict.js
@@ -9,14 +9,15 @@ var PredictScript;
         .GetValue();
     KeyBind = Menu.AddKeyBind(PATH, "Bind", Enum.ButtonCode.KEY_NONE);
     PredictScript.prescript.OnGameStart = function () {
-        if (Engine.OnceAt(1)) { // это чтоб у тебя фпс не просаживался
-            var Heroes = EntitySystem.GetHeroesList(); //если ты это не будешь в энжин засовывать, тебя матвей в жопу выебет
-        }
-        var NearHero = Input.GetNearestHeroToCursor(Enum.TeamType.TEAM_ENEMY); //Вот эта вся хуня снизу получает расстояние до героя возле курсора
+        // это чтоб у тебя фпс не просаживался
+        //если ты это не будешь в энжин засовывать, тебя матвей в жопу выебет
+        //Вот эта вся хуня снизу получает расстояние до героя возле курсора
         var localhero = EntitySystem.GetLocalHero();
         var localplayer = EntitySystem.GetLocalPlayer();
-        var NearHeroPos = NearHero.GetAbsOrigin();
-        if (KeyBind.IsKeyDownOnce()) {
+        var Heroes = EntitySystem.GetHeroesList();
+        if (KeyBind.IsKeyDown()) {
+            var NearHero = Input.GetNearestHeroToCursor(Enum.TeamType.TEAM_ENEMY);
+            var NearHeroPos = NearHero.GetAbsOrigin();
             if (localhero.GetAbsOrigin().Distance(NearHeroPos) <= 1300 && enable) // эта ебатория вычисляет хватает ли дистанции на хук. Если нет - то тебе скажут что ты даун
              {
                 console.log("Получил местоположение героя");
