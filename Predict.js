33231a37a15d0e8b4b85cc0f09821cebcb44c743
Frezidew <bertovroman2006@gmail.com>
2023-08-02 09:13:06 +0300

    Update Predict.js

--- Predict.js
+++ Predict.js
@@ -8,7 +8,7 @@ var PredictScript;
         .SetGeneral(true)
         .GetValue();
     KeyBind = Menu.AddKeyBind(PATH, "Bind", Enum.ButtonCode.KEY_NONE);
-    PredictScript.prescript.OnGameStart = PredictScript.prescript.OnScriptLoad = function () {
+    PredictScript.prescript.OnGameStart = function () {
         if (Engine.OnceAt(1)) { // это чтоб у тебя фпс не просаживался
             var Heroes = EntitySystem.GetHeroesList(); //если ты это не будешь в энжин засовывать, тебя матвей в жопу выебет
         }
@@ -16,13 +16,12 @@ var PredictScript;
         var localhero = EntitySystem.GetLocalHero();
         var localplayer = EntitySystem.GetLocalPlayer();
         var NearHeroPos = NearHero.GetAbsOrigin();
-        if (!enable) {
-            return;
-        }
         if (KeyBind.IsKeyDownOnce()) {
-            if (localhero.GetAbsOrigin().Distance(NearHeroPos) <= 1300 + localhero.GetCastRangeBonus() && enable) { // эта ебатория вычисляет хватает ли дистанции на хук. Если нет - то тебе скажут что ты даун
+            if (localhero.GetAbsOrigin().Distance(NearHeroPos) <= 1300 && enable) // эта ебатория вычисляет хватает ли дистанции на хук. Если нет - то тебе скажут что ты даун
+             {
+                console.log("Получил местоположение героя");
                 localplayer.PrepareUnitOrders(Enum.UnitOrder.DOTA_UNIT_ORDER_CAST_POSITION, null, NearHeroPos, localhero.GetAbility("pudge_meat_hook"), Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_HERO_ONLY, localhero);
-                console.log("Хук нашёл");
+                console.log("Хук нашёл, ренжу получил");
             }
             else {
                 console.log("Хук не нашёл");
